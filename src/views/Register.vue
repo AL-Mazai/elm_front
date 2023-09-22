762d05aa4fe85c6e8898106f1eb4224cf01a5e93
zzw <3279527604@qq.com>
2023-09-22 16:29:57 +0800

    修改注册页面

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,37 +2,37 @@
     <div class="register-container">
         <!--表单部分-->
         <div class="title"><p class="title-font">用户注册</p></div>
-        <el-form :model="registerForm" :rules="rules" ref="registerForm" label-width="80px" style="margin-right: 10vw;">
-            <el-form-item label="手机" prop="phone">
-                <el-input v-model="registerForm.phone" type="tel" placeholder="请输入手机号码"></el-input>
-            </el-form-item>
-            <el-form-item label="密码" prop="password">
-                <el-input v-model="registerForm.password" type="password" placeholder="请输入密码" show-password></el-input>
-            </el-form-item>
-            <el-form-item label="确认密码" prop="checkPassword">
-                <el-input v-model="registerForm.checkPassword" type="password" placeholder="请确认密码" show-password></el-input>
-            </el-form-item>
-            <el-form-item label="性别" prop="sex">
-                <el-radio v-model="registerForm.sex" label="1">男</el-radio>
-                <el-radio v-model="registerForm.sex" label="0">女</el-radio>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" class="login-btn" @click="register('registerForm')">注册</el-button>
-            </el-form-item>
-        </el-form>
-
-        <!--底部菜单-->
-        <Footer></Footer>
+        <div class="register-box">
+            <el-form :model="registerForm" :rules="rules" ref="registerForm" label-width="80px" style="margin-right: 10vw;">
+                <el-form-item label="手机" prop="phone" style="margin-top: 20px">
+                    <el-input v-model="registerForm.phone" type="tel" placeholder="请输入手机号码"></el-input>
+                </el-form-item>
+                <el-form-item label="用户名" prop="username" style="margin-top: 20px">
+                    <el-input v-model="registerForm.username" type="tel" placeholder="请输入用户名"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password">
+                    <el-input v-model="registerForm.password" type="password" placeholder="请输入密码" show-password></el-input>
+                </el-form-item>
+                <el-form-item label="确认密码" prop="checkPassword">
+                    <el-input v-model="registerForm.checkPassword" type="password" placeholder="请确认密码" show-password></el-input>
+                </el-form-item>
+                <el-form-item label="性别" prop="sex">
+                    <el-radio v-model="registerForm.sex" label="1">男</el-radio>
+                    <el-radio v-model="registerForm.sex" label="0">女</el-radio>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" class="login-btn" @click="register('registerForm')">注册</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
 </template>
 
 <script>
-import Footer from "@/components/Footer";
 
 export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name: "Register",
-    components: {Footer},
     data() {
         return {
             registerForm: {
@@ -45,6 +45,9 @@ export default {
                 phone: [
                     { required: true, message: '请输入手机号码', trigger: 'blur' },
                     { pattern: /^1[3456789]\d{9}$/, message: '请输入正确的手机号码', trigger: 'blur' }
+                ],
+                username: [
+                    { required: true, message: '请输入用户名', trigger: 'blur' },
                 ],
                 password: [
                     { required: true, message: '请输入密码', trigger: 'blur' }
@@ -86,6 +89,10 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    background-color: #f8f8f6;
+
+    min-height: 100vh; /* 最小高度为视窗高度 */
 }
 
 .title {
@@ -105,6 +112,16 @@ export default {
     margin-top: 3vw;
     color: white;
     font-size: 4.8vw;
+}
+
+.register-box{
+    margin-top: 20vw;
+    padding: 10px;
+
+    border-radius: 10px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4);
+
+    background-color: #fff; /* 内容区域背景色 */
 }
 
 .login-btn {
